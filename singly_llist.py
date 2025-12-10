f97874a3721314ac264e3772657574158b67b871
mohamedriayzkhan <mohamedriayz14@gmail.com>
2025-12-10 12:32:01 +0530

    File added

---
+++ singly_llist.py
@@ -0,0 +1,50 @@
+class Node:
+    def _init_(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def _init_(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def length(self):
+        count = 0
+        current_node = self.head
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
+
+    def search(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            current_node = current_node.next
+        return False
+
+if __name__ == "_main_":
+    ll = LinkedList()
+    ll.append("A")
+    ll.append("B")
+    ll.append("C")
+    ll.print_list()
+    print("Length:", ll.length())
+    print("Search for 'B':", ll.search("B"))
+    print("Search for 'D':", ll.search("D"))

b59bb374326e18c5ca79612d8be3a62e47a755f9
mohamedriayzkhan <mohamedriayz14@gmail.com>
2025-12-20 12:12:52 +0530

    create file

---
+++ Movie_Sudession_App/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import joblib
+import os
+app = Flask(__name__)
+
+
+model = joblib.load('model.pkl')
+interest_map = joblib.load('interest_map.pkl')
+
+
+interest_map_rev = {}
+for interest, number in interest_map.items():
+    interest_map_rev[number] = interest
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':     
+        age = int(request.form['age'])
+        gender = int(request.form['gender'])
+        pred_num = model.predict([[age, gender]])[0]
+
+        prediction = interest_map_rev.get(pred_num, "Unknown")
+
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Movie_Sudession_App/train_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+
+data = pd.read_csv('/home/intellect/Documents/Data_Scientist/Movie_Sudession_App/Movie_Interests_DecisionTree.csv')
+
+
+interest_names = data['Interest'].unique() 
+interest_map = {} 
+for i in range(len(interest_names)):
+    interest_map[interest_names[i]] = i  
+data['Interest_encoded'] = data['Interest'].map(interest_map)
+
+
+X = data[['Age', 'Gender']] 
+y = data['Interest_encoded']  
+
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+
+joblib.dump(model, 'model.pkl')
+joblib.dump(interest_map, 'interest_map.pkl')
+
+print('Model and mapping saved successfully.')
